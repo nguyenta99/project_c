087538e27d232a174e28361fbdc056e66017edc4
ShiedaKayn1975 <allmid66@gmail.com>
2022-06-12 09:58:37 +0700

    uodate

--- src/App.js
+++ src/App.js
@@ -3,10 +3,19 @@ import './index.css'
 import AppProvider from './AppProvider'
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
+import MetaTags from 'react-meta-tags';
+import fb_icon from './assets/images/facebook_icon.ico'
 
 const App = (props) => {
+
   return (
     <div>
+      <MetaTags>
+        <title>Mua bán tài khoản quảng cáo</title>
+        <meta name="description" content="Dịch vụ mua bán tài khoản quảng cáo Facebook." />
+        <meta property="og:title" content="Mua bán Tài Khoản Quảng Cáo" />
+        <meta property="og:image" content={fb_icon} />
+      </MetaTags>
       <AppProvider
         {...props}
       />
