e468afccf2aaeca2c1f11d1ab559692afb750403
ShiedaKayn1975 <allmid66@gmail.com>
2022-06-12 17:36:41 +0700

    update

--- src/AppProvider.js
+++ src/AppProvider.js
@@ -107,11 +107,11 @@ const components = [
 ]
 
 const loggerStore = store => next => action => {
-  console.group(action.type)
-  console.info('dispatching', action)
+  // console.group(action.type)
+  // console.info('dispatching', action)
   let result = next(action)
-  console.log('next state', store.getState())
-  console.groupEnd()
+  // console.log('next state', store.getState())
+  // console.groupEnd()
   return result
 }
 
